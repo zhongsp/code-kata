a5d06ed0398ab36c1e8df99fa4a5abf1fafd3ba4
zhongsp <patrick.zhongsp@gmail.com>
2018-11-10 21:15:41 +0800

    commit

--- heap/heap.h
+++
@@ -1,8 +0,0 @@
-#ifndef heap_h
-#define heap_h
-
-void max_heapify(int heap[], int heap_size, int root_index);
-
-void print_heap(int heap[], int heap_size);
-
-#endif  // !heap_h
---
+++ heap/heap.ts
@@ -0,0 +1,62 @@
+/**
+ * （二叉）最大堆，数组实现（数组索引从0开始）。
+ */
+
+// 左儿子索引
+const left = (i: number) => 2 * i;
+// 右儿子索引
+const right = (i: number) => 2 * i + 1;
+// 父结点索引
+const parent = (i: number) => Math.floor(i / 2);
+
+interface Heap {
+  size: number;
+  elements: number[];
+}
+
+export function printHeap(heap: Heap): void {
+  const { size, elements } = heap;
+
+  elements.forEach((ele, idx) => {
+    if (idx > 0 && idx <= size) {
+      console.log(ele);
+    }
+  });
+}
+
+/**
+ * 保持最大堆性质。使以索引 i 为根的子树成为最大堆。
+ * 即将当前根结点与其左/右儿子比较，若当前根结点非最大值，
+ * 则令其下降，与最大子结点交换。
+ * 
+ * 当给定结点数为（n）时，堆的最大高度为（lg n）。
+ * 时间复杂度为：O(lg n)。
+ */
+export function maxHeapify(heap: Heap, i: number): void {
+  const { size, elements } = heap;
+
+  // "i" is out of heap index range
+  if (i > size) return;
+
+  let largest = i;
+  if (left(i) <= size && elements[largest] < elements[left(i)]) {
+    largest = left(i);
+  }
+  if (right(i) <= size && elements[largest] < elements[right(i)]) {
+    largest = right(i);
+  }
+
+  if (i !== largest) {
+    [elements[i], elements[largest]] = [elements[largest], elements[i]];
+    maxHeapify(heap, largest);
+  }
+}
+
+const heap = {
+  elements: [-Infinity, 1, 4, 5, 2, 3, 2, 3],
+  size: 7
+};
+
+maxHeapify(heap, 1);
+
+printHeap(heap);
