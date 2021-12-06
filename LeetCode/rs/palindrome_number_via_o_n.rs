378729523ea55d01b0973915b7a3c23c148065af
zhongsp <patrick.zhongsp@gmail.com>
2021-12-06 11:16:19 +0800

    leetcode

--- LeetCode/rs/palindrome_number_via_o_n.rs
+++ LeetCode/rs/palindrome_number_via_o_n.rs
@@ -1,10 +1,31 @@
 //! https://leetcode-cn.com/problems/palindrome-number/
 //!
+//! ## 题目分析
+//!
+//! 第一反应是用字符串操作，因为比较熟悉。
+//! 但题目里不让用字符串，然后就想到了数字反转，之前做过一道整数反转的题：
+//! https://leetcode-cn.com/problems/reverse-integer/。
+//! 需要注意的点就是反转时不能直接反转，因为可能出现数字超出 `i32::MAX` 范围。
+//! 然后想到可以略去一位，后来发现可以只反转一半，利用回文的性质。
 
 struct Solution();
 
 impl Solution {
-    pub fn is_palindrome(x: i32) -> bool {}
+    pub fn is_palindrome(x: i32) -> bool {
+        let mut x = x;
+        let mut y = 0;
+
+        if x < 0 || (x % 10 == 0 && x != 0) {
+            return false;
+        }
+
+        while x > y {
+            y = 10 * y + x % 10;
+            x /= 10
+        }
+
+        x == y || y / 10 == x
+    }
 }
 
 #[cfg(test)]
@@ -59,5 +80,10 @@ mod tests {
     #[test]
     fn test_reverse_9() {
         assert!(Solution::is_palindrome(313));
+    }
+
+    #[test]
+    fn test_reverse_10() {
+        assert!(!Solution::is_palindrome(i32::MAX));
     }
 }
