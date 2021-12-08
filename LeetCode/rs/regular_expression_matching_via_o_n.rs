7df5cf558c27ddea1aad7a6cef82fd57e97e5e18
zhongsp <patrick.zhongsp@gmail.com>
2021-12-08 18:57:28 +0800

    leetcode

--- LeetCode/rs/regular_expression_matching_via_o_n.rs
+++ LeetCode/rs/regular_expression_matching_via_o_n.rs
@@ -1,39 +1,55 @@
+//! https://leetcode-cn.com/problems/regular-expression-matching/solution/zheng-ze-biao-da-shi-pi-pei-by-leetcode-solution/
 //!
+//! # 题目分析
+//!
+//! 最先想到的递归解法，效率不太行，但比较容易理解。
+//! 因为工作中经常会用到正则表达式，所以对识别可能的情况比较清晰。
+//! 其次，是用到了 Stack 的数据结构，有性能损耗，但是理解容易，不用记索引。
+//!
+//! 各种普通、特殊的情况见代码里的测试用例。
 
 struct Solution();
 
 impl Solution {
+    pub fn to_stack(s: &str) -> Vec<char> {
+        s.chars().rev().collect::<Vec<char>>()
+    }
+    pub fn to_string(stack: &[char]) -> String {
+        stack.iter().rev().collect::<String>()
+    }
+
     pub fn is_match(s: String, p: String) -> bool {
-        // setup stack data structure
-        let mut s_stack = s.chars().rev().collect::<Vec<char>>();
-        let mut p_stack = p.chars().rev().collect::<Vec<char>>();
+        let mut s_stack = Solution::to_stack(&s);
+        let mut p_stack = Solution::to_stack(&p);
 
         while let Some(pattern) = p_stack.pop() {
             match pattern {
                 '*' => panic!("A non-'*' character must exist in front of '*'."),
                 '.' => {
-                    if let Some(ch) = p_stack.pop() {
-                        if ch == '*' {
-                            if p_stack.is_empty() {
-                                return true;
-                            } else {
-                                let mut is_match_sub = false;
-                                for _ in 0..=s_stack.len() {
-                                    let sub_match = Solution::is_match(
-                                        s_stack.iter().rev().collect::<String>(),
-                                        p_stack.iter().rev().collect::<String>(),
-                                    );
-                                    if sub_match {
-                                        is_match_sub = true;
-                                        break;
-                                    } else {
-                                        s_stack.pop();
-                                    }
-                                }
-                                return is_match_sub;
-                            }
+                    if p_stack.last() == Some(&'*') {
+                        p_stack.pop();
+
+                        if p_stack.is_empty() {
+                            // when /x.*/
+                            return true;
                         } else {
-                            p_stack.push(ch);
+                            // when /.*xy/
+                            let p_remaining = Solution::to_string(&p_stack);
+
+                            // 递归检查 /.*/ 后面的模式是否匹配任意一段剩余的字符串
+                            while !s_stack.is_empty() {
+                                if Solution::is_match(
+                                    Solution::to_string(&s_stack),
+                                    p_remaining.clone(),
+                                ) {
+                                    return true;
+                                } else {
+                                    s_stack.pop();
+                                }
+                            }
+
+                            // 剩余字符串可能为空字符串
+                            return Solution::is_match(String::new(), p_remaining.clone());
                         }
                     }
 
@@ -42,44 +58,36 @@ impl Solution {
                     }
                 }
                 pattern => {
-                    if let Some(pattern_next) = p_stack.pop() {
-                        if pattern_next == '*' {
-                            if p_stack.is_empty() {
-                                while let Some(ch) = s_stack.pop() {
-                                    if ch == pattern {
-                                        continue;
-                                    } else {
-                                        s_stack.push(ch);
-                                        break;
-                                    }
-                                }
-                            } else {
-                                let mut is_match_sub = false;
-                                while Some(&pattern) == s_stack.last() {
-                                    let sub_match = Solution::is_match(
-                                        s_stack.iter().rev().collect::<String>(),
-                                        p_stack.iter().rev().collect::<String>(),
-                                    );
-                                    if sub_match {
-                                        is_match_sub = true;
-                                        break;
-                                    } else {
-                                        s_stack.pop();
-                                    }
-                                }
-                                return is_match_sub
-                                    || Solution::is_match(
-                                        s_stack.iter().rev().collect::<String>(),
-                                        p_stack.iter().rev().collect::<String>(),
-                                    );
-                            }
+                    if p_stack.last() == Some(&'*') {
+                        p_stack.pop();
 
+                        if p_stack.is_empty() {
+                            // if /x*/, 应该消耗掉字符串所有连续的 'x'。
+                            while s_stack.last() == Some(&pattern) {
+                                s_stack.pop();
+                            }
                             continue;
                         } else {
-                            p_stack.push(pattern_next);
-                        }
+                            let p_remaining = Solution::to_string(&p_stack);
 
-                        
+                            // if /x*yz/，递归检查 /yz/ 是否匹配排除若干个字符 'x' 后的字符串
+                            while Some(&pattern) == s_stack.last() {
+                                if Solution::is_match(
+                                    Solution::to_string(&s_stack),
+                                    p_remaining.clone(),
+                                ) {
+                                    return true;
+                                } else {
+                                    s_stack.pop();
+                                }
+                            }
+
+                            // 检查 /yz/ 是否匹配完全排除字符 'x' 后的字符串
+                            return Solution::is_match(
+                                Solution::to_string(&s_stack),
+                                p_remaining.clone(),
+                            );
+                        }
                     }
 
                     if s_stack.pop() != Some(pattern) {
@@ -89,6 +97,7 @@ impl Solution {
             }
         }
 
+        // 若正则匹配成功，字符串的所有字符应该被消耗
         s_stack.is_empty()
     }
 }
@@ -179,6 +188,14 @@ mod test {
         assert!(Solution::is_match(
             String::from("mississippi"),
             String::from("mis*is*ip*.")
+        ));
+    }
+
+    #[test]
+    fn test_is_match_13() {
+        assert!(Solution::is_match(
+            String::from("abcd"),
+            String::from(".*a*")
         ));
     }
 }
