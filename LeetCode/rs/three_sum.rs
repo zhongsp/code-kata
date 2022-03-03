ce6f01e096a3821c70d90c89dfaa8b0678563c57
zhongsp <patrick.zhongsp@gmail.com>
2022-03-03 22:45:54 +0800

    leetcode

--- LeetCode/rs/three_sum.rs
+++ LeetCode/rs/three_sum.rs
@@ -1,68 +1,74 @@
 //! https://leetcode-cn.com/problems/3sum/
-
-use std::cmp;
-use std::collections::HashMap;
+//!
+//! Given a list of numbers, we want to find out:
+//! a + b + c = 0
 
 struct Solution();
 
 #[allow(dead_code)]
 impl Solution {
     pub fn three_sum(nums: Vec<i32>) -> Vec<Vec<i32>> {
+        let mut nums = nums;
+        nums.sort();
+
         if nums.len() < 3 {
             return vec![];
         }
 
         let mut result: Vec<Vec<i32>> = vec![];
-        let mut table: HashMap<String, bool> = HashMap::new();
 
-        let mut start = 0;
-        let mut end = nums.len();
-        loop {
-            if start >= end {
-                break;
+        let mut last_a = None;
+
+        for ia in 0..nums.len() {
+            let a = nums[ia];
+
+            if last_a == Some(a) {
+                continue;
+            } else {
+                last_a = Some(a);
             }
 
-            Self::sub_three_sum(&nums, &mut result, &mut table, start, end);
+            let mut ib = ia + 1;
+            let mut ic = nums.len() - 1;
 
-            start += 1;
-            end -= 1;
+            let mut last_b = None;
+
+            loop {
+                if ib >= ic {
+                    break;
+                }
+
+                let b = nums[ib];
+
+                if last_b == Some(b) {
+                    ib += 1;
+                    continue;
+                } else {
+                    last_b = Some(b);
+                }
+
+                loop {
+                    if ib >= ic {
+                        break;
+                    }
+
+                    let c = nums[ic];
+
+                    let total = a + b + c;
+                    if total == 0 {
+                        result.push(vec![a, b, c]);
+                        ic -= 1;
+                        break;
+                    } else if total < 0 {
+                        break;
+                    } else {
+                        ic -= 1;
+                    }
+                }
+            }
         }
 
         result
-    }
-
-    fn sub_three_sum(
-        nums: &Vec<i32>,
-        result: &mut Vec<Vec<i32>>,
-        table: &mut HashMap<String, bool>,
-        start: usize,
-        end: usize,
-    ) {
-        let first = nums[start];
-        let last = nums[end - 1];
-        let mut index = start + 1;
-
-        loop {
-            if index >= end - 1 {
-                break;
-            }
-
-            if first + last + nums[index] == 0 {
-                let pair = Self::sort(first, last, nums[index]);
-                if !table.contains_key(&pair.1) {
-                    table.insert(pair.1, true);
-                    result.push(pair.0);
-                }
-            }
-
-            index += 1;
-        }
-    }
-
-    fn sort(x: i32, y: i32, z: i32) -> (Vec<i32>, String) {
-        let min = cmp::min(x, cmp::min(y, z));
-        let max = cmp::max(x, cmp::max(y, z));
-        (vec![min, 0 - min - max, max], format!("{},{}", min, max))
     }
 }
 
@@ -76,5 +82,15 @@ mod test {
             Solution::three_sum(vec![-1, 0, 1, 2, -1, -4]),
             vec![vec![-1, -1, 2], vec![-1, 0, 1]]
         )
+    }
+
+    #[test]
+    fn three_sum1() {
+        assert_eq!(Solution::three_sum(vec![]), vec![] as Vec<Vec<i32>>)
+    }
+
+    #[test]
+    fn three_sum2() {
+        assert_eq!(Solution::three_sum(vec![0]), vec![] as Vec<Vec<i32>>)
     }
 }
