598baf9f6f3e1a852443b097119a4220964062a4
zhongsp <patrick.zhongsp@gmail.com>
2022-05-09 18:38:35 +0800

    leetcode

--- LeetCode/rs/trapping_rain_water.rs
+++ LeetCode/rs/trapping_rain_water.rs
@@ -13,6 +13,7 @@ impl Solution {
             if height[i] == 0 {
                 continue;
             } else {
+                let i = i as i32;
                 if x == -1 {
                     x = i;
                 } else if y == -1 {
@@ -24,9 +25,29 @@ impl Solution {
         }
 
         if x >= 0 {
-            ans += Self::trap(height.into_iter().map(|v| if v >= 1 { v -= 1 } else { 0 }));
-        } else {
-            ans
+            ans += Self::trap(
+                height
+                    .iter()
+                    .map(|&v| if v >= 1 { v - 1 } else { 0 })
+                    .collect(),
+            );
         }
+
+        ans
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn trap_0() {
+        assert_eq!(Solution::trap(vec![0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]), 6)
+    }
+
+    #[test]
+    fn trap_1() {
+        assert_eq!(Solution::trap(vec![0, 1]), 0)
     }
 }
