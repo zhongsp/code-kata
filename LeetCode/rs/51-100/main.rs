dfb92c7d606a45e50a728ce9088dae31e3859f70
zhongsp <patrick.zhongsp@gmail.com>
2022-05-25 11:04:12 +0800

    leetcode

---
+++ LeetCode/rs/51-100/main.rs
@@ -0,0 +1,3 @@
+mod unique_substrings_in_wraparound_string;
+
+fn main() {}
---
+++ LeetCode/rs/51-100/unique_substrings_in_wraparound_string.rs
@@ -0,0 +1,23 @@
+//! https://leetcode-cn.com/problems/combination-sum-ii/
+
+struct Solution;
+
+#[allow(dead_code)]
+impl Solution {
+    pub fn find_substring_in_wrapround_string(p: String) -> i32 {
+        0
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn is_valid0() {
+        assert_eq!(
+            Solution::find_substring_in_wrapround_string(String::from("abc")),
+            0
+        )
+    }
+}
